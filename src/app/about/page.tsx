05a8dc66d8aa5fb6c4d470e205d4585cced14183
Sanjay <sanjaytech63@gmail.com>
2025-05-10 05:02:32 +0530

    first commit

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,7 +4,6 @@ export default function AboutPage() {
   return (
     <section className="bg-black mt-10 text-white py-20 px-4 sm:px-6 lg:px-10 max-w-7xl mx-auto">
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
-        {/* Image Section */}
         <div className="w-full">
           <img
             src="https://pagedone.io/asset/uploads/1717741205.png"
@@ -13,16 +12,14 @@ export default function AboutPage() {
           />
         </div>
 
-        {/* Info Section */}
         <div className="flex flex-col gap-8">
           <div className="space-y-4 text-center lg:text-left">
-            <h2 className="text-3xl sm:text-4xl font-bold">Hi, I'm Sanjay Choudhary</h2>
+            <h2 className="text-3xl sm:text-4xl font-bold">Hi Im Sanjay Choudhary</h2>
             <p className="text-gray-400 text-base sm:text-lg leading-relaxed">
-              I'm a passionate <span className="text-indigo-400">Frontend Developer</span> experienced in building responsive, high-performance web apps using <strong>React</strong>, <strong>Next.js</strong>, and <strong>Tailwind CSS</strong>.
+              Im a passionate <span className="text-indigo-400">Frontend Developer</span> experienced in building responsive, high-performance web apps using <strong>React</strong>, <strong>Next.js</strong>, and <strong>Tailwind CSS</strong>.
             </p>
           </div>
 
-          {/* Stats Section */}
           <div className="flex flex-wrap justify-center lg:justify-start gap-6 mt-4">
             <div className="text-center min-w-[100px]">
               <h3 className="text-3xl font-bold">1.5+</h3>
@@ -38,14 +35,12 @@ export default function AboutPage() {
             </div>
           </div>
 
-          {/* Contact Info */}
           <div className="bg-white/5 border border-white/10 backdrop-blur-sm rounded-xl p-6 shadow-md space-y-3 text-sm text-gray-300">
             <p><span className="text-white font-semibold">Name:</span> Sanjay Choudhary</p>
             <p><span className="text-white font-semibold">Email:</span> sjat6375697673@gmail.com</p>
-            <p><span className="text-white font-semibold">Phone:</span> 6376024125, 9376893981</p>
+            <p><span className="text-white font-semibold">Phone:</span> 6376024125 - 9376893981</p>
           </div>
 
-          {/* Button */}
           <div>
             <Link href={"/"}>
               <button className="w-full cursor-pointer sm:w-fit px-6 py-3 bg-white rounded-lg text-black text-sm font-semibold shadow-md transition duration-300">
